5fd6d42a4e75ad0497b3c599389c921a406d3db9
Ian Shaffer <towafius@gmail.com>
2024-11-02 16:14:08 -0500

    Added plating animation

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -27,11 +27,15 @@ func _process(delta: float) -> void:
 		move(Vector2(0,-1))
 	elif (Input.is_action_pressed("down")):
 		move(Vector2(0,1))
-	elif (Input.is_action_pressed("water")):
-		if tile_detector.tile_data && animation_player.current_animation!="water" && current_energy>0 && !tile_detector.tile_data.get_custom_data("is_watered"):
+	elif (Input.is_action_pressed("1")):
+		if tile_detector.tile_data && animation_player.current_animation=="idle" && current_energy>0 && !tile_detector.tile_data.get_custom_data("is_watered"):
 			current_energy-=1
 			tile_detector.current_tilemaplayer.set_cell(tile_detector.tile_coords, 0, Vector2i(5, 0))
 			animation_player.play("water")
+	elif (Input.is_action_pressed("2") || Input.is_action_pressed("3") || Input.is_action_pressed("4")):
+		if tile_detector.tile_data && animation_player.current_animation=="idle" && current_energy>0 && tile_detector.tile_data.get_custom_data("plant_type") == "empty":
+			animation_player.play("planting")
+		
 	move_and_slide()
 
 	
