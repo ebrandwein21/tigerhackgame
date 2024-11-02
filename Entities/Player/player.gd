21e7343e54b24fb0d2af5c90da92ce76b6fc76fe
Ian Shaffer <towafius@gmail.com>
2024-11-01 22:48:08 -0500

    Some Player Animations

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -2,19 +2,25 @@ extends CharacterBody2D
 
 @export var move_amount:int = 16
 
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_player.play("idle")
 
 func _unhandled_input(event: InputEvent) -> void:
 	if (event.is_action_pressed("right")):
 		global_position.x += move_amount
-	if (event.is_action_pressed("left")):
+	elif (event.is_action_pressed("left")):
 		global_position.x += -move_amount
-	if (event.is_action_pressed("up")):
+	elif (event.is_action_pressed("up")):
 		global_position.y += -move_amount
-	if (event.is_action_pressed("down")):
+	elif (event.is_action_pressed("down")):
 		global_position.y += move_amount
+	elif (event.is_action_pressed("water")):
+		animation_player.play("water")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
