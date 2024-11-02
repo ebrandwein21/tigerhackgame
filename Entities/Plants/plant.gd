8c587b7e097aa33a8e9dd93361698461e2419c8a
Ian Shaffer <towafius@gmail.com>
2024-11-02 16:47:16 -0500

    Plant class made

---
+++ Entities/Plants/plant.gd
@@ -0,0 +1,17 @@
+class_name Plant
+extends Node2D
+
+@export var grow_time:int = 0
+@export var yield_amount:float = 0
+@export var water_needed:int = 0
+@export var plant_stage:int = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
