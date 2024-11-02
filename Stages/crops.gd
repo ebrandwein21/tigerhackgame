a6bc8bb0811cdb610f68fdd12e9f467cd18c121b
tri <2015iann@gmail.com>
2024-11-02 11:48:53 -0500

    crops properties

--- Stages/crops.gd
+++ Stages/crops.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+var Name = ""
+var Cost = 0
+var Water = 0
+var Yield = 0
+var WeedProne = false
+var FastGro = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
